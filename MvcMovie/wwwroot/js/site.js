8b79c51954e649b022b2a87be71599867487963f
Ambes <52856270+ambes77@users.noreply.github.com>
2020-09-09 00:04:57 +0200

    Add project files.

---
+++ MvcMovie/wwwroot/js/site.js
@@ -0,0 +1,10 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+$('.nav-item').on('click', function (e) {
+    $('.nav-item').removeClass('active');
+    console.log(e);
+    $(e.currentTarget).addClass('active')
+});
